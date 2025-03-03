a1473765e8ae3a6dd9e538dd69f5a618428a7952
Solvercks3 <daniestevelozano@gmail.com>
2025-03-03 16:09:11 +0000

    [🌙] Backup realizado 2025-03-03

---
+++ servidor_minecraft/backups/restore-script.bat
@@ -0,0 +1,5 @@
+@echo off 
+/:
+cd "/workspaces/servid/servidor_minecraft/mods"
+java -jar "AdvancedBackups-forge-1.20-3.7.1.jar"
+pause
---
+++ servidor_minecraft/backups/restore-script.sh
@@ -0,0 +1,2 @@
+cd "/workspaces/servid/servidor_minecraft/mods"
+java -jar "AdvancedBackups-forge-1.20-3.7.1.jar"
